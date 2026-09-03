304eecb4d66369961c8913f6a0333328f709af12
Kebo <kebokyo@eleboog.com>
2026-09-03 17:31:20 -0500

    actually following the CDDL whoops

--- src/app/widgets/list_work_items.rs
+++ src/app/widgets/list_work_items.rs
@@ -1,3 +1,24 @@
+// CDDL HEADER START
+//
+// The contents of this file are subject to the terms of the
+// Common Development and Distribution License (the "License").
+// You may not use this file except in compliance with the License.
+//
+// You can obtain a copy of the license in the file LICENSE
+// or at https://opensource.org/licenses/CDDL-1.0.
+// See the License for the specific language governing permissions
+// and limitations under the License.
+//
+// When distributing Covered Code, include this CDDL HEADER in each
+// file and include the License file. If applicable, add the following
+// below this CDDL HEADER, with the fields enclosed by brackets "[]"
+// replaced with your own identifying information:
+// Portions Copyright [yyyy] [name of copyright owner]
+//
+// CDDL HEADER END
+//
+// Copyright 2026 millie.moe. All rights reserved.
+// Use is subject to license terms.
 //! One project's work items.
 
 use std::sync::{Arc, RwLock};
